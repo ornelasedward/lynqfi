16b5d0531f43e2c678ea41c2eb3df99decd8a4cc
Edward Ornelas <ornelasedward@rocketmail.com>
2025-06-19 21:34:08 -0500

    fixing env

--- lib/particleClient.ts
+++ lib/particleClient.ts
@@ -4,6 +4,6 @@ import { mainnet } from "viem/chains"
 export const particleClient = createPublicClient({
   chain: mainnet,
   transport: http(
-    `https://rpc.particle.network/evm-chain?chainId=1&projectUuid=${process.env.NEXT_PARTICLE_NETWORK_PUBLIC_PROJECT_ID}&clientKey=${process.env.NEXT_PARTICLE_NETWORK_PUBLIC_CLIENT_KEY}`
+    `https://rpc.particle.network/evm-chain?chainId=1&projectUuid=${process.env.NEXT_PUBLIC_PARTICLE_NETWORK_PROJECT_ID}&clientKey=${process.env.NEXT_PUBLIC_PARTICLE_NETWORK_CLIENT_KEY}`
   ),
 })
